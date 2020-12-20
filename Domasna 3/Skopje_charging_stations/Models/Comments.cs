dbd08fca722d3406e13d0d885e2e78cf092077d9
LeonaIlieva <lea.leona13@gmail.com>
2020-12-20 17:20:42 +0100

    Added Database File

---
+++ Domasna 3/Skopje_charging_stations/Models/Comments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Skopje_charging_stations.Models
+{
+    public class Comments
+    {
+        [Key]
+        public String comment { get; set; }
+    }
+}
